76eb91a30a4e9c008bddb68f3898e2c913865821
Sergey Pugach <sp@voir.me>
2020-08-21 15:45:13 +0300

    Initial Commit

---
+++ CI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CI
+//
+//  Created by Sergey Pugach on 8/21/20.
+//  Copyright © 2020 Sergey Pugach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
